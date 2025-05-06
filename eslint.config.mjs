1914ea04d497648da16c1ea2a0ab93b6a49b19f3
worksofliam <mrliamallan@live.co.uk>
2025-05-06 10:20:58 -0400

    Add script to fix indentation and auto-fixable problems
    Signed-off-by: worksofliam <mrliamallan@live.co.uk>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,4 +6,9 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
+  {
+    rules: {
+      indent: ['error', 2], // Enforce 2-space indentation
+    },
+  }
 );
